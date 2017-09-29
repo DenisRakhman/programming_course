7909a96b4a8f85805f89c6f05c4c5b12fc121353
DenisRakhman <drahman2@mail.ru>
2017-09-29 22:45:56 +0300

    Update task_3.py

--- homework_2/task_3.py
+++ homework_2/task_3.py
@@ -50,12 +50,14 @@ class TextStatistics:
         import collections as cl
         grams_freq = {}
         for article in self.articles:
-            for s in range(0, len(article)-3):
-                cur_gram = article[s:s+3]
-                if cur_gram not in grams_freq:
-                    grams_freq[cur_gram] = 1
-                else:
-                    grams_freq[cur_gram] += 1
+            words = article.split()
+            for w in range(0, len(words)):
+                if w >= 2:
+                    cur_gram = words[w-2] + ' ' + words[w-1] + ' ' + words[w]
+                    if cur_gram not in grams_freq:
+                        grams_freq[cur_gram] = 1
+                    else:
+                        grams_freq[cur_gram] += 1
         grams_freq = cl.OrderedDict(sorted(grams_freq.items(), key=lambda t: t[1], reverse=1))
         list_of_3grams_in_descending_order_by_freq = [gram for gram in grams_freq]
         list_of_their_corresponding_freq = [grams_freq[gram] for gram in grams_freq]
@@ -87,81 +89,81 @@ class Experiment:
         self.stats = TextStatistics(self.articles)
         self.grams = self.stats.get_top_3grams()
         self.words = self.stats.get_top_words()
+        self.top_grams_out = [' - '.join([self.grams[0][i], str(self.grams[1][i])]) for i in range(20)]
         self.top_grams = self.grams[0][:20]
+        self.top_words_out = [' - '.join([self.words[0][i], str(self.words[1][i])]) for i in range(20)]
         self.top_words = self.words[0][:20]
 
         nlp = Wikipedia().article('Natural_language_processing')
         nlptext = plaintext(nlp.source)
+        parse = WikiParser()
+        nlptext = parse.normalize(nlptext)
         self.nlp_stats = TextStatistics([nlptext])
-        self.nlp_top_grams = self.nlp_stats.get_top_3grams()[0][:5]
-        self.nlp_top_words = self.nlp_stats.get_top_words()[0][:5]
+        self.nlp_top_grams = [' - '.join([self.nlp_stats.get_top_3grams()[0][i], str(self.nlp_stats.get_top_3grams()[1][i])]) for i in range(20)][:5]
+        self.nlp_top_words = [' - '.join([self.nlp_stats.get_top_words()[0][i], str(self.nlp_stats.get_top_words()[1][i])]) for i in range(20)][:5]
+
 
 
     def show_results(self):
-        out  = 'Experiment results: \n\n Most frequent 3-grams throughout the corpus:\n' + '\n'.join(self.top_grams) +'\n\nMost frequent words throughout the corpus\n' + '\n'.join(self.top_words)
+        out  = 'Experiment results: \n\n Most frequent 3-grams throughout the corpus:\n' + '\n'.join(self.top_grams_out) +'\n\nMost frequent words throughout the corpus\n' + '\n'.join(self.top_words_out)
         out += '\n\n Most frequent 3-grams throughout NLP article:\n' + '\n'.join(self.nlp_top_grams)+'\n\nMost frequent words throughout the NLP article\n' + '\n'.join(self.nlp_top_words)
         print (out)
 
 
-
-# Experiment results:
-#
-# Most frequent 3 - grams throughout the corpus:
-# th
-# the
-# he
-# ion
-# in
-# on
-# ed
-# ing
-# tio
-# of
-# an
-# of
-#
-# ng
-# es
-# ati
-# nd
-# and
-# co
-# al
+#  Most frequent 3-grams throughout the corpus:
+# natural language processing - 336
+# from the original - 306
+# archived from the - 296
+# v t e - 277
+# the use of - 238
+# the original on - 238
+# as well as - 224
+# one of the - 205
+# a b c - 186
+# proceedings of the - 182
+# the european union - 163
+# cambridge university press - 158
+# of the european - 155
+# such as the - 153
+# the number of - 143
+# a number of - 141
+# university press isbn - 140
+# for example the - 136
+# a set of - 131
+# based on the - 130
 #
 # Most frequent words throughout the corpus
-# and
-# to
-# is
-# that
-# are
-# language
-# or
-# be
-# it
-# this
-# which
-# can
-# not
-# was
-# english
-# speech
-# retrieved
-# languages
-# such
-# words
+# and - 16876
+# to - 11980
+# is - 8688
+# that - 4823
+# are - 4302
+# language - 4074
+# or - 3847
+# be - 3398
+# it - 2735
+# this - 2528
+# which - 2205
+# can - 1966
+# not - 1954
+# was - 1805
+# english - 1801
+# speech - 1739
+# retrieved - 1722
+# languages - 1715
+# such - 1700
+# words - 1668
 #
-# Most frequent 3 - grams throughout NLP article:
-# ing
-# th
-# the
-# ion
-# ng
+#  Most frequent 3-grams throughout NLP article:
+# natural language processing - 16
+# a chunk of - 6
+# chunk of text - 6
+# of natural language - 5
+# systems based on - 4
 #
 # Most frequent words throughout the NLP article
-# and
-# to
-# is
-# language
-# such
-
-
+# and - 72
+# language - 59
+# to - 54
+# is - 48
+# natural - 39
